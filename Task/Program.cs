18439540031c47f7d98b8c2eaada83cb62335df7
Vitaliy Antipov <demjke64@gmail.com>
2023-07-24 00:01:32 +0400

    Программа

---
+++ Task/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] arr1 = new string[5] { "asdsadas", "as", "asd", "fasfasdada", "12" };
+string[] arr2 = new string[arr1.Length];
+
+void SecondArr(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            arr2[count] = arr1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArr(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+SecondArr(arr1, arr2);
+PrintArr(arr2);
